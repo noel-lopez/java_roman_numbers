d70869882c4961bdf81768e979825a9e869002a4
Noel Lopez <nlopez.publi@gmail.com>
2024-03-15 17:17:49 +0100

    feature: convert 5 into V

--- src/main/java/com/codurance/RomanConverter.java
+++ src/main/java/com/codurance/RomanConverter.java
@@ -3,6 +3,9 @@ package com.codurance;
 public class RomanConverter {
 
     public static String convert(int number) {
+        if (number == 5) {
+            return "V";
+        }
         String romanNumeral = "";
         for (int i = 0; i < number; i++) {
             romanNumeral += "I";
