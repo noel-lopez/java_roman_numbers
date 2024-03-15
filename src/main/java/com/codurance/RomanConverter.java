b7495c8c0598a7e7cd3669ea640ccb66b225f378
Noel Lopez <nlopez.publi@gmail.com>
2024-03-15 15:01:37 +0100

    feature: return II given 2

--- src/main/java/com/codurance/RomanConverter.java
+++ src/main/java/com/codurance/RomanConverter.java
@@ -3,6 +3,9 @@ package com.codurance;
 public class RomanConverter {
 
     public static String convert(int i) {
+        if (i == 2) {
+            return "II";
+        }
         return "I";
     }
 }
